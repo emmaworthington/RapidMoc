2c8f0a476f9fa12aa1e81fd66ba610d522b480c5
emmaworthington <elw2u16@soton.ac.uk>
2020-10-01 08:41:30 +0100

    Code cleaned

--- rapidmoc/rapidmoc.py
+++ rapidmoc/rapidmoc.py
@@ -6,8 +6,6 @@ Module containing main routines to execute RapidMoc
 
 import argparse
 import configparser
-import copy
-
 
 from . import sections
 from . import transports
@@ -16,31 +14,45 @@ from . import plotdiag
 
 
 def get_args():
-    """   Get arguments from command line.  """
+    """
+    Get arguments from command line.
+    """
+
     parser = argparse.ArgumentParser(
         description='Calculate RAPID AMOC diagnostics using ocean model data')
     parser.add_argument(
-        'config_file', type=str, help='Path to configuration file.')
+        'config_file', type=str,
+        help='Path to configuration file.')
     parser.add_argument(
-        'tfile', type=str, help='Path for netcdf file(s) containing temperature data.')
+        'tfile', type=str,
+        help='Path for netcdf file(s) containing temperature data.')
     parser.add_argument(
-        'sfile', type=str, help='Path for netcdf file(s) containing salinity data.')
+        'sfile', type=str,
+        help='Path for netcdf file(s) containing salinity data.')
     parser.add_argument(
-        'taufile', type=str, help='Path for netcdf file(s) containing zonal wind stress data.')
+        'taufile', type=str,
+        help='Path for netcdf file(s) containing zonal wind stress data.')
     parser.add_argument(
-        'vfile', type=str, help='Path for netcdf file(s) containing meridional velocity data.')
+        'vfile', type=str,
+        help='Path for netcdf file(s) containing meridional velocity data.')
     parser.add_argument(
-        '--name', help='Name used in output files. Overrides value in config file.', default=None)
+        '--name',
+        help='Name used in output files. Overrides value in config file.',
+        default=None)
     parser.add_argument(
-        '--outdir', help='Path used for output data. Overrides value in config file.', default=None)
+        '--outdir',
+        help='Path used for output data. Overrides value in config file.',
+        default=None)
     args = parser.parse_args()
 
     return args
 
- 
 
 def get_config(args):
-    """ Return configuration options as <ConfigParser> object. """
+    """
+    Return configuration options as <ConfigParser> object.
+    """
+
     config = configparser.ConfigParser()
     config.read(args.config_file)
 
@@ -56,7 +68,7 @@ def get_config_opt(config, section, option):
 
 
 def call_plotdiag(config, trans):
-    """ Call plotting routines to compare against RAPID observations """ 
+    """ Call plotting routines to compare against RAPID observations """
 
     # Initialize observations
     obs_fc, obs_oht, obs_vol, obs_sf = None, None, None, None
@@ -67,7 +79,7 @@ def call_plotdiag(config, trans):
     obs_fc_f = get_config_opt(config, 'observations', 'florida_current')
     obs_vol_f = get_config_opt(config, 'observations', 'volume_transports')
     obs_oht_f = get_config_opt(config, 'observations', 'heat_transports')
-    
+
     # Load observations, if specified
     if obs_oht_f is not None:
         obs_oht = observations.HeatTransportObs(obs_oht_f, time_avg=time_avg)
@@ -80,13 +92,15 @@ def call_plotdiag(config, trans):
 
     if obs_vol_f is not None:
         obs_vol = observations.VolumeTransportObs(obs_vol_f, time_avg=time_avg)
- 
+
     # Call plot routines
     outdir = config.get('output', 'outdir')
     date_format = config.get('output', 'date_format')
     name = config.get('output', 'name')
-    plotdiag.plot_diagnostics(trans, name=name, outdir=outdir, date_format=date_format,
-                              obs_vol=obs_vol, obs_fc=obs_fc, obs_oht=obs_oht, obs_sf=obs_sf)
+    plotdiag.plot_diagnostics(trans, name=name, outdir=outdir,
+                              date_format=date_format,
+                              obs_vol=obs_vol, obs_fc=obs_fc,
+                              obs_oht=obs_oht, obs_sf=obs_sf)
 
 
 def main():
@@ -111,16 +125,15 @@ def main():
     # Interpolate T & S data onto v-grid
     t_on_v = sections.interpolate(t, v)
     s_on_v = sections.interpolate(s, v)
-  
+
     # Return integrated transports on RAPID section as netcdf object
     trans = transports.calc_transports_from_sections(
         config, v, tau, t_on_v, s_on_v)
-    
-    ## Plot diagnostics
-    if config.getboolean('output','plot'):
+
+    # Plot diagnostics
+    if config.getboolean('output', 'plot'):
         call_plotdiag(config, trans)
-        
+
     # Write data
     print('SAVING: %s' % trans.filepath())
     trans.close()
-
